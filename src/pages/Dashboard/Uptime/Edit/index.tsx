298baf15b2076472adb248865c2f70d8971f7dd9
Dan Chen <dan.chen@boardware.com>
2023-09-13 15:55:49 +0800

    feat

--- src/pages/Dashboard/Uptime/Edit/index.tsx
+++ src/pages/Dashboard/Uptime/Edit/index.tsx
@@ -10,32 +10,29 @@ const Create: React.FC = () => {
   const navigate = useNavigate();
   const { id } = useParams();
   const { monitor } = useLoaderData() as { monitor: Monitor };
+
   return (
-    <>
-      {monitor && (
-        <MonitorForm
-          bodyForm={monitor.body ? monitor.body.form : undefined}
-          acceptedStatusCodes={monitor.acceptedStatusCodes}
-          name={monitor.name}
-          description={monitor.description}
-          interval={monitor.interval}
-          timeout={monitor.timeout}
-          url={monitor.url}
-          headers={monitor.headers}
-          notificationInterval={monitor.notificationInterval}
-          to={monitor.notifications[0].emailReceivers?.to[0]}
-          title="Edit monitor"
-          onEmit={(request) => {
-            monitorApi
-              .updateMonitor({ id: id!, putMonitorRequest: request })
-              .then(() => {
-                enqueueSnackbar("Monitor updated.", { variant: "success" });
-                navigate("/dashboard/uptime/monitors/" + id);
-              });
-          }}
-        />
-      )}
-    </>
+    <MonitorForm
+      bodyForm={monitor.body ? monitor.body.form : undefined}
+      acceptedStatusCodes={monitor.acceptedStatusCodes}
+      name={monitor.name}
+      description={monitor.description}
+      interval={monitor.interval}
+      timeout={monitor.timeout}
+      url={monitor.url}
+      headers={monitor.headers}
+      notificationInterval={monitor.notificationInterval}
+      to={monitor.notifications[0].emailReceivers?.to[0]}
+      title="Edit monitor"
+      onEmit={(request) => {
+        monitorApi
+          .updateMonitor({ id: id!, putMonitorRequest: request })
+          .then(() => {
+            enqueueSnackbar("Monitor updated.", { variant: "success" });
+            navigate("/dashboard/uptime/monitors/" + id);
+          });
+      }}
+    />
   );
 };
 
